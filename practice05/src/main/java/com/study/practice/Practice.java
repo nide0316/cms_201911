b731515513bfdb1de65b33699c8929c3640b423f
Your Name <you@example.com>
2019-12-04 23:00:36 +0800

    第一次提交

---
+++ practice05/src/main/java/com/study/practice/Practice.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.study.practice;
+
+/**
+ * @author：阿宁
+ *
+ * 2019年12月4日
+ */
+public class Practice {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+	}
+}
