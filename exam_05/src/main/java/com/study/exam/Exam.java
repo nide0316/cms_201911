7dc91387e773aeab6694bd2efbc811e9de2804c4
Your Name <you@example.com>
2019-12-05 09:10:25 +0800

    第一次提交

---
+++ exam_05/src/main/java/com/study/exam/Exam.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.study.exam;
+
+/**
+ * @author：阿宁
+ *
+ * 2019年12月5日
+ */
+public class Exam {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+	}
+}
